67c5588a3224fe61d1c3d4857eb6973ce6e84e9a
Mrcubix <tintamare91@gmail.com>
2021-07-25 06:55:41 +0200

    Fixed group

--- Gess1t's Area Randomizer/Clients.cs
+++ Gess1t's Area Randomizer/Clients.cs
@@ -17,20 +17,20 @@ namespace Area_Randomizer
         public async Task StartAsync()
         {
             client = new NamedPipeClientStream(".", pipename, PipeDirection.InOut, PipeOptions.Asynchronous | PipeOptions.WriteThrough | PipeOptions.CurrentUserOnly);
-            Log.Debug("AreaVisualizer", $"Waiting for connections...");
+            Log.Debug("AreaRandomizer", $"Waiting for connections...");
             await client.ConnectAsync();
-            Log.Debug("AreaVisualizer", $"Connected = {client.IsConnected}");
+            Log.Debug("AreaRandomizer", $"Connected = {client.IsConnected}");
             rpc = JsonRpc.Attach(client);
 
             rpc.Disconnected += (_, _) =>
             {
-                Log.Debug("AreaVisualizer", $"Server disconnected.");
+                Log.Debug("AreaRandomizer", $"Server disconnected.");
                 client.Dispose();
                 rpc.Dispose();
                 _ = StartAsync();
             };
 
-            Log.Debug("AreaVisualizer", $"Now listening to {pipename}");
+            Log.Debug("AreaRandomizer", $"Now listening to {pipename}");
         }
         public void Dispose()
         {
